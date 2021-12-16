c26c87c1116b1418300740f89c85da4d376ab2c3
Juanita Keaton <juanitahalle613@email.arizona.edu>
2021-12-15 22:46:57 -0800

    8-10 answer

--- practicum/week-2/question10.js
+++ practicum/week-2/question10.js
@@ -8,5 +8,9 @@ output: 7 - 2 = 5
 */
 
 var maxMinusMin = function(n) {
-
+    if (n < 0) n = -1 * n;   
+    else return 0;
+    let min = 9, max = 0;
+    if (n > 0);
+    return max - min;
 };
--- practicum/week-2/question8.js
+++ practicum/week-2/question8.js
@@ -8,5 +8,14 @@ output: [1, 0, 1, 2]
 */
 
 var biggerAndEven = function(input) {
-
+    let result = [];
+    for (let i = 0; i < input.length; i ++) {
+        let count = 0;
+        for (let j = 0; j < input.length; j++) {
+            if (input[j] > input[i] && input[j] % 2 === 0)
+                ++count;
+        }
+        result[i] = count;
+    }
+    return result;
 };
